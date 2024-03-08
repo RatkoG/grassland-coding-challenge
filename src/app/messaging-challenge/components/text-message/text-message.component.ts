6b40eeed8d4b4b333c9c92bfea1e7c95aaeeebdb
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2024-03-08 11:09:48 -0600

    chore: Messaging Goal 2

---
+++ src/app/messaging-challenge/components/text-message/text-message.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { TextMessage } from '../../models/text-message';
+import { BaseMessageComponent } from '../base-message/base-message.component';
+
+@Component({
+  selector: 'app-text-message',
+  templateUrl: './text-message.component.html',
+  styleUrls: ['./text-message.component.scss']
+})
+export class TextMessageComponent extends BaseMessageComponent  {
+  @Input() message: TextMessage;
+  textSend: boolean;
+ 
+  ngOnInit(): void {
+    this.textSend = this.message.from === 'Anna';
+  }
+}
