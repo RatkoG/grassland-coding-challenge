e2c613e7688e73d4c05adb934d681720b7110c5b
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2024-03-08 10:22:25 -0600

    chore: Messaging Goal 1

--- src/app/messaging-challenge/components/messaging/messaging.component.ts
+++ src/app/messaging-challenge/components/messaging/messaging.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Message } from '../../models/message';
+import { MessagingService } from '../../services/messaging.service';
 
 @Component({
   selector: 'app-messaging',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./messaging.component.scss']
 })
 export class MessagingComponent {
+  private subscription: Subscription;
+  messages: Message[];
+  constructor(private messagingService: MessagingService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.messagingService.messages$.subscribe((messages: Message[]) => {
+      this.messages = messages;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
 }
